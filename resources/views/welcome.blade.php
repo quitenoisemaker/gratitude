bdea34cf3a651f1239e7cf3711029eadc24b705b
samson <samsonojugo@gmail.com>
2022-02-02 16:59:27 +0100

    minor changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,31 +65,16 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                    @endauth
-                </div>
-            @endif
+          
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Gratitude
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/question') }}">Click here</a>
+                    
                 </div>
             </div>
         </div>
